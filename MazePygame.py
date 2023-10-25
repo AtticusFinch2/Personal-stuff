047cd78f3ff475e261a7f03874b3f57c53f3f21b
AtticusFinch2 <bradyspymiller71@gmail.com>
2023-10-25 11:49:52 -0500

    woo

--- MazePygame.py
+++ MazePygame.py
@@ -2,31 +2,113 @@ import pygame
 import random as rand
 from collections import defaultdict
 # pygame setup
-pygame.init()
 xScene = 1280
 yScene = 720
-screen = pygame.display.set_mode((xScene, yScene))
-clock = pygame.time.Clock()
-running = True
-dt = 0
 boxW = 32
 boxH = 32
+borderW = 1
 cols = int(xScene/boxW)+1
 rows = int(yScene/boxH)+1
-borderW = 1
-squareColor = [[pygame.Color(0,0,0) for x in range(rows)] for y in range(cols)]
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
 edges = defaultdict(set)
-edges[(1,1)] = {(0,1)}
+#edges[(1,1)] = {(0,1)} possible maze implementation later
 dirs = [(0,1), (1,0), (0,-1), (-1,0)]
-def boxDrawer(): #changes squareColor array into a grid with each color on that square
+stateSquare = [["Unclicked" for x in range(rows)] for y in range(cols)]
+global BLACK
+BLACK = pygame.color.Color('black') # usually a global
+
+global cellValue
+cellValue = [[0 for x in range(rows)] for y in range(cols)]
+
+defaultNotClickedColor = pygame.Color(100,100,100)
+defaultClickedColor = pygame.Color(0,100,0)
+defaultHoveringColor = pygame.Color(50,50,50)
+global squareColor
+squareColor = [[defaultNotClickedColor for x in range(rows)] for y in range(cols)]
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((xScene, yScene))
+    clock = pygame.time.Clock()
+    running = True
+    global font
+    font = pygame.font.Font(None, 36)
+    dt = 0
+    ticker=0
+    LastClick = 0
+    curTime =0
+    click=False
+    player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+    while running:
+        # poll for events
+        # pygame.QUIT event means the user clicked X to close your window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                click = True
+            if event.type == pygame.MOUSEBUTTONUP:
+                click = False
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_q]:
+                running = False
+        # fill the screen with a color to wipe away anything from last frame
+        screen.fill("purple")
+        (x,y) = pygame.mouse.get_pos()
+        col = int(x / boxW)
+        row = int(y / boxH)
+        curTime = curTime+1
+        if click and curTime>LastClick+5:
+            print(col, row, (x,y))
+            cellValue[col][row] +=1
+            if stateSquare[col][row] == "Clicked":
+                stateSquare[col][row] = "Unclicked"
+                squareColor[col][row] = defaultNotClickedColor
+            else:
+                stateSquare[col][row] = "Clicked"
+                squareColor[col][row] = defaultClickedColor
+            boxDrawer(screen)
+            LastClick = curTime
+            print(LastClick)
+        else:
+            if stateSquare[col][row] == "Clicked":
+                squareColor[col][row] = defaultHoveringColor
+                boxDrawer(screen)
+                squareColor[col][row] = defaultClickedColor
+                stateSquare[col][row] = "Clicked"
+            else:
+                squareColor[col][row] = defaultHoveringColor
+                boxDrawer(screen)
+                squareColor[col][row] = defaultNotClickedColor
+                stateSquare[col][row] = "Unclicked"
+        wallDrawer(screen)
+
+
+        keys = pygame.key.get_pressed()
+
+        # flip() the display to put your work on screen
+        pygame.display.flip()
+
+        # limits FPS to 60
+        # dt is delta time in seconds since last frame, used for framerate-
+        # independent physics.
+        # I don't use this, but it is useful to have if i want to make smooth movements later on
+        dt = clock.tick(60) / 1000
+
+    pygame.quit()
+def boxDrawer(screen): #changes squareColor array into a grid with each color on that square
     for col in range(cols):
         for row in range(rows):
-            pygame.draw.rect(screen, pygame.Color(0, 100, 0), pygame.Rect((boxW*col,boxH*row), (boxW,boxH)), width=borderW)
+            pygame.draw.rect(screen, pygame.Color(0, 100, 0), pygame.Rect((boxW*col,boxH*row), (boxW,boxH)), width=borderW)#green borders, draw over with red if there is wall
             topLeftX = boxW*col+borderW
             topLeftY = boxH*row+borderW
-            pygame.draw.rect(screen, squareColor[col][row], pygame.Rect((topLeftX,topLeftY), (boxW-(2*borderW),boxH-(2*borderW))))
-def wallDrawer():
+            theCell = pygame.Rect((topLeftX,topLeftY), (boxW-(2*borderW),boxH-(2*borderW)))
+            pygame.draw.rect(screen, squareColor[col][row], theCell)
+            text = font.render(f"{cellValue[col][row]}", False, BLACK)
+            textPos = theCell
+            screen.blit(text, textPos)
+
+def wallDrawer(screen):
     for col in range(cols):
         for row in range(rows):
             for i in range(4):
@@ -40,75 +122,6 @@ def wallDrawer():
                         topLeft = (boxW*col, boxH*row)
                         endPos = (topLeft[0] + (boxW+borderW)*(dir[0]), topLeft[1] + (boxH+borderW)*(dir[1]))
                     pygame.draw.line(screen, pygame.Color(100, 0, 0), topLeft, endPos, width=borderW)
-stateSquare = [["Unclicked" for x in range(rows)] for y in range(cols)]
-ticker=0
-LastClick = 0
-curTime =0
-click=False
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            click = True
-        if event.type == pygame.MOUSEBUTTONUP:
-            click = False
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_q]:
-            running = False
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
-    boxDrawer()
-    wallDrawer()
-    (x,y) = pygame.mouse.get_pos()
-    col = int(x / boxW)
-    row = int(y / boxH)
-    curTime = curTime+1
-    if click and curTime>LastClick+3:
-        print(col, row, (x,y))
-        if stateSquare[col][row] == "Clicked":
-            stateSquare[col][row] = "Unclicked"
-            squareColor[col][row] = pygame.Color(0, 0, 0)
-        else:
-            stateSquare[col][row] = "Clicked"
-            squareColor[col][row] = pygame.Color(100, 100, 100)
-        boxDrawer()
-        LastClick = curTime
-        print(LastClick)
-    else:
-        if stateSquare[col][row] == "Clicked":
-            squareColor[col][row] = pygame.Color(50, 50, 50)
-            boxDrawer()
-            squareColor[col][row] = pygame.Color(100,100,100)
-            stateSquare[col][row] = "Clicked"
-        else:
-            squareColor[col][row] = pygame.Color(50, 50, 50)
-            boxDrawer()
-            squareColor[col][row] = pygame.Color(0, 0, 0)
-            stateSquare[col][row] = "Unclicked"
-    wallDrawer()
 
 
-    keys = pygame.key.get_pressed()
-
-    '''if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt'''
-
-    # flip() the display to put your work on screen
-    pygame.display.flip()
-
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    dt = clock.tick(60) / 1000
-
-pygame.quit()
-
+main()
