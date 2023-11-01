1c799e0a1659a59f4335f4c5e57dcd9d921c1626
AtticusFinch2 <bradyspymiller71@gmail.com>
2023-10-31 19:16:25 -0500

    Update makeGrid.py

--- makeGrid.py
+++ makeGrid.py
@@ -1,22 +1,26 @@
 def make_grid(width:int, height:int):
     i=0
-    ans = {j:[] for j in range(width*height)}
-    for row in range(height):
-        for col in range(width):
-            print(row, col, i)
-            if col == width-1 and row == height-1:
+    ans = {j:set() for j in range(width*height)}
+    for col in range(width):
+        for row in range(height):
+            lastCol = i % width >= width - 1
+            lastRow = i//width >= height -1
+            #print(i, lastRow, lastCol)
+            if lastRow and lastCol:
+                #print("end")
                 pass
-            elif row == height - 1:
-                ans[i] = [i + 1]
-                ans[i + 1].append(i)
-            elif col == width - 1:
-                ans[i] = [i + width]
-                ans[i + width].append(i)
+            elif lastCol:
+                ans[i].add(i+width)
+                ans[i+width].add(i)
+            elif lastRow:
+                ans[i + 1].add(i)
+                ans[i].add(i+1)
             else:
-                ans[i] = [i+1, i+width]
-                ans[i+1].append(i)
-                ans[i+width].append(i)
-            i+=1
-
-    return ans
-print(make_grid(3,2))
+                ans[i].add(i + 1)
+                ans[i].add(i + width)
+                ans[i+1].add(i)
+                ans[i+width].add(i)
+            i += 1
+    ansFinal = {key: [edge for edge in ans[key]] for key in ans}
+    return ansFinal
+print( make_grid( 3 , 2 ) )
