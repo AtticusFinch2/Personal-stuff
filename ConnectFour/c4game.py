7ad06fe8172e18314a9f390224b4557c5fc76dbc
AtticusFinch2 <bradyspymiller71@gmail.com>
2023-12-12 17:11:43 -0600

    working game

---
+++ ConnectFour/c4game.py
@@ -0,0 +1,157 @@
+import pygame
+import c4logic
+import numpy as np
+from dataclasses import dataclass
+
+# window setup
+xScene = 700
+yScene = 600
+framerate = 15
+cols = 7
+rows = 6
+boxW = int(xScene / cols)
+boxH = int(yScene / rows)
+borderW = 5
+boxradius = 10
+dirs = [(0, -1), (-1, 0), (0, 1), (1, 0)]
+borderColor = pygame.Color(122, 122, 110)
+bgColor = pygame.Color(245, 245, 220)
+
+
+@dataclass
+class Model:
+    running: bool = True
+
+
+class ModelData:
+    def __init__(
+        self,
+        screen=pygame.display.set_mode((xScene, yScene)),
+        board=np.array([[0, 0, 0, 0, 0, 0] for _ in range(7)]),
+    ):
+        self.x = -10
+        self.y = -10
+        self.screen = screen
+        self.turn = 1
+        self.board = board
+        self.click = False
+        self.hoverSq = (-1, -1)
+        self.player = 1
+
+
+def main():
+    pygame.init()
+    model = ModelData()
+    clock = pygame.time.Clock()
+    running = True
+    dt = 0
+    global fontbig, fontsmall
+    fontbig = pygame.font.Font(None, 150)
+    fontsmall = pygame.font.Font(None, 100)
+    while running:
+        # poll for events
+        # pygame.QUIT event means the user clicked X to close your window
+        (model.x, model.y) = pygame.mouse.get_pos()
+        hover_handler(model)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                model.click = True
+            if event.type == pygame.MOUSEBUTTONUP:
+                model.click = False
+            if event.type == pygame.KEYDOWN:
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_q]:
+                    running = False
+                if keys[pygame.K_SPACE]:
+                    movehandler(model)
+        # fill the screen with a color to wipe away anything from last frame
+        model.screen.fill(pygame.Color(0, 0, 250))
+
+        drawHandler(model)
+        drawWinner(model)
+
+        # flip() the display to put your work on screen
+        pygame.display.flip()
+
+        # limits FPS to 60
+        # dt is delta time in seconds since last frame, used for framerate-
+        # independent physics.
+        # I don't use this, but it is useful to have if i want to make smooth movements later on
+        dt = clock.tick(framerate) / 1000
+
+
+def hover_handler(model):
+    col = int(model.x / boxW)
+    row = int(model.y / boxH)
+    if col >= cols:
+        col = cols - 1
+    elif row >= rows:
+        row = rows - 1
+    if model.board[col][row] > 0:
+        model.hoverSq = (-1, -1)
+    else:
+        model.hoverSq = (col, row)
+
+
+def flip(player):
+    if player == 1:
+        return 2
+    return 1
+
+
+def movehandler(model):
+    move = c4logic.placeMoveFast(model.board, model.hoverSq[0], model.player)
+    if move[0] != -1:
+        print(move)
+        model.board[move[0]][move[1]] = model.player
+        model.player = flip(model.player)
+
+
+cfv = {
+    0: pygame.Color(0, 0, 0),
+    1: pygame.Color(250, 0, 0),
+    2: pygame.Color(200, 200, 0),
+    3: pygame.Color(50, 0, 0),
+    4: pygame.Color(50,50,0)
+}
+
+
+def drawHandler(model):
+    for c in range(cols):
+        for r in range(rows):
+            pygame.draw.circle(
+                model.screen,
+                cfv[model.board[c][r]],
+                ((boxW * c + (boxW / 2)), (boxW * r + (boxW / 2))),
+                boxW // 2.5,
+            )
+    move = c4logic.placeMoveFast(model.board, model.hoverSq[0], model.player)
+    pygame.draw.circle(
+        model.screen,
+        cfv[model.player+2],
+        (
+            (boxW * move[0] + (boxW / 2)),
+            (boxW * move[1] + (boxW / 2)),
+        ),
+        boxW // 2.5,
+    )
+
+cfw = {1:"Red", 2:"Yellow"}
+def drawWinner(model):
+    winner = c4logic.wins(model.board)
+    if winner > 0:
+        topLeft = (0, 0)
+        theCell = pygame.Rect(
+            topLeft, (model.screen.get_width(), model.screen.get_height())
+        )
+        pygame.draw.rect(model.screen, pygame.Color(0, 0, 0), theCell)
+        text = fontsmall.render(
+            f"{cfw[winner]} won!", False, pygame.Color(250, 250, 250)
+        )
+        textPos = text.get_rect(center=(xScene / 2, yScene / 2))
+        model.screen.blit(text, textPos)
+
+
+main()
--- ConnectFour/c4logic.py
+++ ConnectFour/c4logic.py
@@ -1,52 +1,119 @@
 import copy
-import random
 import numpy as np
-# we store a board as 6x7 numPy array and
-ex_board = np.array([[0,0,0,0,1,2,1]for _ in range(6)])
+
+# we store a board as 7x6 numPy array and
+ex_board = np.array([[0, 0, 0, 1, 2, 1] for _ in range(6)])
 
 
 def placeMove(board, colPlaced, player):
-    if board[colPlaced][0] !=0:
+    if board[colPlaced][0] != 0:
         return board
     new_board = copy.deepcopy(board)
-    for i in range(7):
+    for i in range(6):
         if new_board[colPlaced][i] != 0:
-            new_board[colPlaced][i-1] = player
+            new_board[colPlaced][i - 1] = player
     return new_board
 
 
-def placeMoveFast(board, colPlaced, player):  # Not memory safe but doesn't use deepcopy
+def placeMoveNonCopy(board, colPlaced, player):
     if board[colPlaced][0] != 0:
         return board
-    for i in range(7):
+    for i in range(6):
         if board[colPlaced][i] != 0:
             board[colPlaced][i - 1] = player
     return board
 
 
+def placeMoveFast(board, colPlaced, player):  # returns where it goes, not new board
+    if board[colPlaced][0] != 0:  # can't make move
+        return (colPlaced, -1)
+    for i in range(6):
+        if board[colPlaced][i] != 0:
+            return (colPlaced, i - 1)
+    return (colPlaced, 5)
+
+
 def wins(board):
-    # rows
-    for row in range(board.shape[0]):
+    for row in range(board.shape[0]):  # check rows
         for col in range(board.shape[1] - 3):
-            if board[row, col] == board[row, col + 1] == board[row, col + 2] == board[row, col + 3] != 0:
-                return True
-
-    # cols
-    for col in range(board.shape[1]):
+            if (
+                1
+                == board[row, col]
+                == board[row, col + 1]
+                == board[row, col + 2]
+                == board[row, col + 3]
+                != 0
+            ):
+                return 1
+            if (
+                2
+                == board[row, col]
+                == board[row, col + 1]
+                == board[row, col + 2]
+                == board[row, col + 3]
+                != 0
+            ):
+                return 2
+    for col in range(board.shape[1]):  # cols
         for row in range(board.shape[0] - 3):
-            if board[row, col] == board[row + 1, col] == board[row + 2, col] == board[row + 3, col] != 0:
-                return True
-
-    # diagonals (from top-left to bottom-right)
-    for row in range(board.shape[0] - 3):
+            if (
+                1
+                == board[row, col]
+                == board[row + 1, col]
+                == board[row + 2, col]
+                == board[row + 3, col]
+                != 0
+            ):
+                return 1
+            if (
+                2
+                == board[row, col]
+                == board[row + 1, col]
+                == board[row + 2, col]
+                == board[row + 3, col]
+                != 0
+            ):
+                return 2
+    for row in range(board.shape[0] - 3):  # diagonals (from top-left to bottom-right)
         for col in range(board.shape[1] - 3):
-            if board[row, col] == board[row + 1, col + 1] == board[row + 2, col + 2] == board[row + 3, col + 3] != 0:
-                return True
-
-    # diagonals pt.2 electric boogaloo (from top-right to bottom-left)
-    for row in range(board.shape[0] - 3):
+            if (
+                1
+                == board[row, col]
+                == board[row + 1, col + 1]
+                == board[row + 2, col + 2]
+                == board[row + 3, col + 3]
+                != 0
+            ):
+                return 1
+            if (
+                2
+                == board[row, col]
+                == board[row + 1, col + 1]
+                == board[row + 2, col + 2]
+                == board[row + 3, col + 3]
+                != 0
+            ):
+                return 2
+    for row in range(
+        board.shape[0] - 3
+    ):  # diagonals pt.2 electric boogaloo (from top-right to bottom-left)
         for col in range(3, board.shape[1]):
-            if board[row, col] == board[row + 1, col - 1] == board[row + 2, col - 2] == board[row + 3, col - 3] != 0:
-                return True
-
-    return False
+            if (
+                1
+                == board[row, col]
+                == board[row + 1, col - 1]
+                == board[row + 2, col - 2]
+                == board[row + 3, col - 3]
+                != 0
+            ):
+                return 1
+            if (
+                2
+                == board[row, col]
+                == board[row + 1, col - 1]
+                == board[row + 2, col - 2]
+                == board[row + 3, col - 3]
+                != 0
+            ):
+                return 2
+    return 0
