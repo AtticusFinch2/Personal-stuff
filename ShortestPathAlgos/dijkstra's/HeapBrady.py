549c8b0a9ecb86bc197532494885e657ba326781
AtticusFinch2 <bradyspymiller71@gmail.com>
2024-01-24 10:09:58 -0600

    heapBuilt

---
+++ ShortestPathAlgos/dijkstra's/HeapBrady.py
@@ -0,0 +1,129 @@
+
+
+class maxHeapBM:
+    def __init__(self):
+        self.list = []
+        self.size = 0
+
+    def printHeap(self):
+        print([self.list[i] for i in range(self.size)])
+
+    def destroyHeap(self):
+        self.list = []
+        self.size = 0
+
+    def swap(self, p, c):
+        self.list[p], self.list[c] = self.list[c], self.list[p]
+
+    def parentValue(self, i):
+        return self.list[(i-1) // 2]
+
+    def children(self, i):
+        return self.list[2 * i + 1], self.list[2 * i + 2]
+
+    def shuffle(self, i):
+        parent = i
+        left_child = 2 * i + 1
+        right_child = 2 * i + 2
+        if left_child < self.size and self.list[left_child] > self.list[parent]:
+            self.swap(parent, left_child)
+            self.shuffle(left_child)
+        if right_child < self.size and self.list[right_child] > self.list[parent]:
+            self.swap(parent, right_child)
+            self.shuffle(right_child)
+
+    def insert(self, value):
+        child = self.size
+        self.size += 1
+        self.list.append(value)  # value at index child
+        # now fix the heap upwards:
+        while child > 0 and self.list[child] > self.parentValue(child):
+            parent = (child-1)//2
+            self.swap(child, parent)
+            child = parent
+
+    def empty(self):
+        return self.size == 0
+
+    def pop(self):
+        if self.size == 0:
+            print("CANNOT DELETE FROM AN EMPTY HEAP")
+            return
+        max_val = self.list[0]
+        # put the last value at the front and then re-sort
+        self.list[0] = self.list.pop()
+        self.size -= 1
+        self.shuffle(0)
+        return max_val
+
+
+class minHeapBM:
+    def __init__(self):
+        self.list = []
+        self.size = 0
+
+    def printHeap(self):
+        print([self.list[i] for i in range(self.size)])
+
+    def destroyHeap(self):
+        self.list = []
+        self.size = 0
+
+    def swap(self, p, c):
+        self.list[p], self.list[c] = self.list[c], self.list[p]
+
+    def parentValue(self, i):
+        return self.list[(i-1) // 2]
+
+    def children(self, i):
+        return self.list[2 * i + 1], self.list[2 * i + 2]
+
+    def empty(self):
+        return self.size == 0
+
+    def shuffle(self, i):
+        parent = i
+        left_child = 2 * i + 1
+        right_child = 2 * i + 2
+        if left_child < self.size and self.list[left_child] < self.list[parent]:
+            self.swap(parent, left_child)
+            self.shuffle(left_child)
+        if right_child < self.size and self.list[right_child] < self.list[parent]:
+            self.swap(parent, right_child)
+            self.shuffle(right_child)
+
+    def insert(self, value):
+        child = self.size
+        self.size += 1
+        self.list.append(value)  # value at index child
+        # now fix the heap upwards:
+        while child > 0 and self.list[child] < self.parentValue(child):
+            parent = (child-1)//2
+            self.swap(child, parent)
+            child = parent
+
+    def pop(self):
+        if self.size == 0:
+            print("CANNOT DELETE FROM AN EMPTY HEAP")
+            return
+        max_val = self.list[0]
+        # put the last value at the front and then re-sort
+        self.list[0] = self.list[self.size-1]
+        self.size -= 1
+        self.shuffle(0)
+        return max_val
+
+'''
+queue_before = [10,35,33,42,10,14,19,27,44,26,31]
+heap = maxHeapBM()
+print(f"Values before insertion into the heap: {queue_before}")
+for element in queue_before:
+    heap.insert(element)
+print("Heap elements before deletion:", end=" ")
+heap.printHeap()
+top_value = heap.pop()
+print("Top element:", top_value)
+print("Heap elements after deletion:", end=" ")
+heap.printHeap()
+heap.destroyHeap()
+'''
--- ShortestPathAlgos/dijkstra's/dijkstraLogic.py
+++ ShortestPathAlgos/dijkstra's/dijkstraLogic.py
@@ -1,5 +1,6 @@
 import random, math, copy
 from queue import PriorityQueue
+from HeapBrady import minHeapBM
 
 adjListEx = {0:{1,7}, 1:{0,2,7}, 2:{1,3,5,8}, 3:{2,4,5}, 4:{3,5}, 5:{2,3,4,6}, 6:{5,7,8}, 7:{0,1,8,6}, 8:{2,6,7}}
 weightsEx = {(0,1):4, (0,7):8, (1,0):4, (1,2):8, (1,7):11, (2,1):8, (2,3):7,
@@ -10,7 +11,7 @@ weightsEx = {(0,1):4, (0,7):8, (1,0):4, (1,2):8, (1,7):11, (2,1):8, (2,3):7,
 # I DID NOT USE geeksforgeeks FOR THE CODE, ONLY FOR THE GRAPH
 
 
-class weightedGraph:
+class weightedGraph:  # directed as well, but for this example there is redundancy
     def __init__(self, weights, edges={}, adj={}):  # weights: {(Node1, Node2) : weight}
         self.edges = edges
         self.weights = weights
@@ -23,28 +24,28 @@ class weightedGraph:
             self.adj[edge[1]].add(edge[0])
         self.n = len(adj)
     def dijkstra(self, start):
-        stack = PriorityQueue()
-        stack.put((0, start, [start]))  # (dist, node, path)
+        heap = minHeapBM()
+        heap.insert((0, start, [start]))  # (dist, node, path)
         shortest_dist_to = {node: (math.inf, []) for node in range(self.n)}  # distance then path for each node
         shortest_dist_to[start] = (0, [start])
-        while not stack.empty():
-            (distToCurrent, current, path_to_current) = stack.get()
+        while not heap.empty():
+            (distToCurrent, current, path_to_current) = heap.pop()
             if distToCurrent <= shortest_dist_to[current][0]:
-                # if it's a slower path, just ignore it. This is the optimization of dijkstra's
+                # if it's a slower path, just ignore it. This is the optimization of dijkstra's over brute force
                 shortest_dist_to[current] = (distToCurrent, path_to_current)
                 for neighbor in self.adj[current]:
                     d_to_neighbor = distToCurrent+(self.weights[(current, neighbor)])
                     # distance to neighbor is just distance to current and the weight of the edge between them
                     path_to_neighbor = path_to_current + [neighbor]
-                    stack.put((d_to_neighbor, neighbor, path_to_neighbor))
+                    heap.insert((d_to_neighbor, neighbor, path_to_neighbor))
         return shortest_dist_to
     def dijkstra_end(self, start, end):
-        stack = PriorityQueue()
-        stack.put((0, start, [start]))  # (dist, node, path)
+        heap = minHeapBM()
+        heap.insert((0, start, [start]))  # (dist, node, path)
         shortest_dist_to = {node: (math.inf, []) for node in range(self.n)}  # distance then path for each node
         shortest_dist_to[start] = (0, [start])
-        while not stack.empty():
-            (distToCurrent, current, path_to_current) = stack.get()
+        while not heap.empty():
+            (distToCurrent, current, path_to_current) = heap.pop()
             if distToCurrent <= shortest_dist_to[current][0]:
                 # if it's a slower path, just ignore it. This is the optimization of dijkstra's
                 shortest_dist_to[current] = (distToCurrent, path_to_current)
@@ -54,7 +55,7 @@ class weightedGraph:
                     d_to_neighbor = distToCurrent+(self.weights[(current, neighbor)])
                     # distance to neighbor is just distance to current and the weight of the edge between them
                     path_to_neighbor = path_to_current + [neighbor]
-                    stack.put((d_to_neighbor, neighbor, path_to_neighbor))
+                    heap.insert((d_to_neighbor, neighbor, path_to_neighbor))
         return shortest_dist_to[end]
 
     def a_star(self, start, end: (float, float)):
@@ -63,8 +64,30 @@ class weightedGraph:
         # now we reformat:
         for edge in self.weights:
             self.new_weights[edge] = self.weights[edge] + potential(edge[0]) - potential(edge[1])
-        return self.dijkstra_end(start, end)
+
+        # now dijkstra's
+        heap = PriorityQueue()
+        heap.insert((0, start, [start]))  # (dist, node, path)
+        shortest_dist_to = {node: (math.inf, []) for node in range(self.n)}  # distance then path for each node
+        shortest_dist_to[start] = (0, [start])
+        while not heap.empty():
+            (distToCurrent, current, path_to_current) = heap.pop()
+            if distToCurrent <= shortest_dist_to[current][0]:
+                # if it's a slower path, just ignore it. This is the optimization of dijkstra's
+                shortest_dist_to[current] = (distToCurrent, path_to_current)
+                if current == end:
+                    break
+                for neighbor in self.adj[current]:
+                    d_to_neighbor = distToCurrent + (self.new_weights[(current, neighbor)])
+                    # distance to neighbor is just distance to current and the weight of the edge between them
+                    path_to_neighbor = path_to_current + [neighbor]
+                    heap.insert((d_to_neighbor, neighbor, path_to_neighbor))
+        return shortest_dist_to[end]
 
 g = weightedGraph(weightsEx)
+print(g.dijkstra(0))
 print(g.dijkstra_end(0,4))
+
+
+
 
